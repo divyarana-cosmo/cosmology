d7afe70e137d11e89f4094e5a6811c1f91cd6eef
Divya Rana <divya.rana@niser.ac.in>
2019-09-24 22:28:44 +0530

    first mcmc code

---
+++ mcmc.py
@@ -0,0 +1,8 @@
+# define pdf = prior * likelihood
+# use gaussian likelihood
+# and a gaussian handle - to get the new position from old position.
+# use the variable used in the hoggs - data analysis recipes - mcmc analysis
+# run for some steps.
+import numpy as np
+
+
